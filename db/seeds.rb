107d747cd29d367b731e0104927bf45a5688b395
Steven Foster <steven@stevenfoster.net>
2023-01-02 13:21:52 -0800

    adding logic to load in writers seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+file = File.read('lib/assets/writers.json')
+writers_hash = JSON.parse(file)
+writers_hash.each do |w|
+	writer = Writer.new(name: w['name'])
+	if w['feed']
+		writer.feed = w['feed']
+	end
+	if w['website']
+		writer.website = w['website']
+	end
+	if w['substack']
+		writer.substack = w['substack']
+	end
+	if w['twitter']
+		writer.twitter = w['twitter']
+	end
+	if w['mentor']
+		writer.mentor = true
+	end
+	if w['staff'] 
+		writer.staff = true
+	end
+	writer.save
+end
