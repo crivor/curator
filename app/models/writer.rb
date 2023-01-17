0e7a76fe099ee6e14716592d9606b731e2ddca0e
Steven Foster <steven@stevenfoster.net>
2023-01-17 12:27:47 -0800

    writer method to fetch new post

--- app/models/writer.rb
+++ app/models/writer.rb
@@ -8,4 +8,17 @@ class Writer < ApplicationRecord
 		posts.sort_by(&:pubdate).last
 	end
 
+	def fetch_new_posts
+		begin
+			URI.open(feed) do |rss|
+				feed = RSS::Parser.parse(rss)
+				feed.items.each do |item|
+					Post.create(title: item.title, pubdate: item.pubDate, link: item.link, writer_id: id)
+				end
+			end
+		rescue OpenURI::HTTPError
+			puts "request failed for #{name}"
+		end
+	end
+
 end
