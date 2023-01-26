fc6aa26d4941b9b798ffbe5d17aab64d40dcfe5b
Steven Foster <steven@stevenfoster.net>
2023-01-25 18:58:12 -0800

    adding tests for minimum and maximum name length

--- test/models/writer_test.rb
+++ test/models/writer_test.rb
@@ -14,4 +14,14 @@ class WriterTest < ActiveSupport::TestCase
     @writer.name = " "
     assert_not @writer.valid?
   end
+
+  test "name should be at least two characters" do
+    @writer.name = "a"
+    assert_not @writer.valid?
+  end
+
+  test "name should not be longer than fifty five chacaters" do
+    @writer.name = "a" * 56
+    assert_not @writer.valid?
+  end
 end
