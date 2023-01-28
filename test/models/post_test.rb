9a3d709d034dd3547060a0772ddd64535b3cda0f
Steven Foster <steven@stevenfoster.net>
2023-01-27 19:59:00 -0800

    setup post tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @writer = Writer.second
+    @post = @writer.posts.build(title: "Test Post", pubdate: Time.now, link: "https://stevenfoster.substack.com/p/test")
+  end
+
+  test "should be valid" do
+    assert @post.valid?
+  end
+
+  test "post should have writer_id" do
+    @post.writer_id = nil
+    assert_not @post.valid?
+  end
+
 end
